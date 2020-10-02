60cd747467b46a917d89692c80968d241f90c416
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-10-02 20:41:12 +0530

    linear_search.py

---
+++ linear_search.py
@@ -0,0 +1,29 @@
+from array import *
+arr=array('i',[])  # empty array
+a=int(input("enter the size of an array:"))
+for j in range(a):
+    b=int(input("enter the value %d" %(j)))
+    arr.append(b)
+
+print("the values of an array are:")
+
+for k in arr:
+    print(k)
+
+key=int(input("enter the key value:"))
+m=0
+for j in arr:
+     if(key==j):
+         m=1
+         break
+
+
+if(m==1):
+    print("search succesfull")
+else:
+ print("search unsuccesfull")
+
+
+
+
+
