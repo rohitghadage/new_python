8cc9503b19f8ad5bac7bed758771081a746bb521
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-10-02 11:59:18 +0530

    exception_handling.py

---
+++ exception_handling.py
@@ -0,0 +1,15 @@
+a=5
+b=0
+
+try:
+    print("start")
+    x=int(input("enter the number"))
+    print(x)
+    print(a/b)
+    #print("start")
+except ZeroDivisionError as e:
+    print("we cant divide number by zero",e)
+except ValueError as e:
+    print("invalid input",e)
+finally:
+    print("done")
